300b749a8d3e02c4414774923a61895419cc3824
brianA1998 <brian.alaniz@hotmail.com>
2022-08-31 21:31:33 -0300

    Fix dependecy with sdk version android

---
+++ app/src/main/java/com/example/jobsapplication/ui/view/home/HomeScreenFragment.kt
@@ -0,0 +1,15 @@
+package com.example.jobsapplication.ui.view.home
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.jobsapplication.R
+
+
+class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
