4f45240776286d4c042e4ae831edfd23ab28c099
brianA1998 <brian.alaniz@hotmail.com>
2022-09-08 00:07:36 -0300

    Data source implements and also repositories

--- app/src/main/java/com/example/jobsapplication/domain/home/HomeRepository.kt
+++ app/src/main/java/com/example/jobsapplication/domain/home/HomeRepository.kt
@@ -1,9 +1,11 @@
 package com.example.jobsapplication.domain.home
 
 import com.example.jobsapplication.data.model.Publication
+import kotlinx.coroutines.flow.Flow
+import com.example.jobsapplication.core.Result
 
 interface HomeRepository {
 
-    suspend fun getAllPublications() : Result<List<Publication>>
+    suspend fun getAllPublications(): Flow<Result<List<Publication>>>
 
 }
--- app/src/main/java/com/example/jobsapplication/domain/home/HomeRepositoryImpl.kt
+++ app/src/main/java/com/example/jobsapplication/domain/home/HomeRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.example.jobsapplication.domain.home
 
+import com.example.jobsapplication.core.Result
 import com.example.jobsapplication.data.model.Publication
+import com.example.jobsapplication.data.remote.home.HomeRemoteDataSource
+import kotlinx.coroutines.flow.Flow
 
-class HomeRepositoryImpl : HomeRepository {
-    override suspend fun getAllPublications(): Result<List<Publication>> {
-        TODO("Not yet implemented")
+
+class HomeRepositoryImpl(private val  dataSource : HomeRemoteDataSource) : HomeRepository {
+    override suspend fun getAllPublications(): Flow<Result<List<Publication>>> {
+        return dataSource.getAllPublications()
     }
 }
