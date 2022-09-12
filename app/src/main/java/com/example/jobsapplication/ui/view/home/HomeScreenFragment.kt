684d10d4c946df08f389357dfe7bc6176053724c
brianA1998 <brian.alaniz@hotmail.com>
2022-09-12 01:25:01 -0300

    Creacion de adapter y baseviewholder

--- app/src/main/java/com/example/jobsapplication/ui/view/home/HomeScreenFragment.kt
+++ app/src/main/java/com/example/jobsapplication/ui/view/home/HomeScreenFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.jobsapplication.R
+import com.example.jobsapplication.databinding.FragmentHomeScreenBinding
 
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
 
+    private lateinit var binding : FragmentHomeScreenBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeScreenBinding.bind(view)
+
     }
 
 }
